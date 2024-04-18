dc29df2218a395702af5b44879439aaf27fd5dc3
MissLavender <51702517+MissLavender-LQ@users.noreply.github.com>
2024-04-18 07:30:12 +0100

    Create virtualbox.nix

---
+++ nixos/virtualbox.nix
@@ -0,0 +1,12 @@
+{
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = ["user-with-access-to-virtualbox"];
+
+  nixpkgs.config.allowUnfree = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+
+  environment.systemPackages = [
+    pkgs.virtualbox
+  ];
+}
